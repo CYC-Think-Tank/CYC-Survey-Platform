9f7b9c6741f22abe52ec256463987ad7998e5ee5
Ryan Xing <ryanxing06@gmail.com>
2026-06-03 11:45:41 -0400

    feat: local DB mode for testing against cloned supabase

--- api/dependencies.py
+++ api/dependencies.py
@@ -3,9 +3,14 @@ import os
 from dotenv import load_dotenv
 from supabase import Client, create_client
 
+from api.local_supabase import LocalSupabaseClient
+
 load_dotenv(dotenv_path=".env.local")
 
-SUPABASE_URL = os.environ.get("SUPABASE_URL")
-SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+LOCAL_DB = os.environ.get("LOCAL_DB")
+if LOCAL_DB:
+    supabase = LocalSupabaseClient(LOCAL_DB)
+else:
+    SUPABASE_URL = os.environ.get("SUPABASE_URL")
+    SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+    supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
